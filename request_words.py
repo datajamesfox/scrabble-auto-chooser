64a91a5ccf4d132c624138fdef168c5b4f43b78b
datajamesfox <75384370+datajamesfox@users.noreply.github.com>
2023-11-11 16:52:31 +0000

    Save file

--- request_words.py
+++ request_words.py
@@ -1,13 +1,18 @@
 import os
 import requests
 
-r = requests.get('https://www.wordgamedictionary.com/sowpods/download/sowpods.txt')
-print(r.headers)
-print(r.headers['content-type'])
-print(r.encoding)
-word_list = r.text.splitlines()
+# -- Dev Notes --
+# Add correct documentation 
+# Create class
+# Ensure request and fiel save are seperate processes
 
-print(word_list)
+
+r = requests.get('https://www.wordgamedictionary.com/sowpods/download/sowpods.txt')
+# print(r.headers)
+# print(r.headers['content-type'])
+# print(r.encoding)
+# word_list = r.text.splitlines()
+# word_list = word_list[6:]
 
 with open(os.path.join('.', 'words.txt'), 'wb') as fd:
     fd.write(r.content)
